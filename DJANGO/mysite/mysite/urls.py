0ea89539893f34b4c436e099fdf333c35d91e0b8
hum4noidx <hum4noid@yandex.ru>
2022-04-09 13:47:49 +0300

    Delete DJANGO directory

--- DJANGO/mysite/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path(r'grappelli/', include('grappelli.urls')),
-    path('', include(('main.urls', 'main'), namespace='main')),
-    path('admin/', admin.site.urls),
-
-]
